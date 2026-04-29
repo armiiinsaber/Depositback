f15585d399fcf83bee1be368c47262057fe1372d
armiiinsaber <armin.sm2@gmail.com>
2026-04-29 15:21:37 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,13 @@ export default function Document() {
   return (
     <Html style={{margin:0,padding:0,background:'#08121f'}}>
       <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-PMG1FCF45T"></script>
+        <script dangerouslySetInnerHTML={{__html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-PMG1FCF45T');
+        `}} />
         <style>{`
           *, *::before, *::after { box-sizing: border-box; }
           html, body, #__next { margin: 0 !important; padding: 0 !important; background: #08121f !important; overflow-x: hidden; }
